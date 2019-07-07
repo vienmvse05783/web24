7f061189159129c7fa3f69573eff6bc31874ff12
Mai Vien <maivien@MacBook-Pro-6.local>
2019-07-07 09:06:37 +0700

    homework

--- homeWorkSlot3/quyetde/public/js/main.js
+++ homeWorkSlot3/quyetde/public/js/main.js
@@ -1,2 +1,18 @@
-// query element object 
-//event oninput
+const maxLength = 200;
+document.getElementById("textarea").maxLength = maxLength;
+
+countCharacter = () => {
+    var textInput = document.getElementById("textarea").value.length;
+    var numLess = maxLength - textInput;
+    document.getElementById("numChar").innerHTML = numLess;
+}
+
+checkBlank = () => {
+    var textInput = document.getElementById("textarea").value.trim().length;
+    if(textInput == 0){
+        document.getElementById("error").hidden = false;
+        document.getElementById("error").innerHTML = "Not Blank!";
+    } else {
+        document.getElementById("error").hidden = true;
+    }
+}
